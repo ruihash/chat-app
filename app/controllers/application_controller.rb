121f6044b2cded3937308f2304b0d750e7a46bbf
ruihash <rstoyo.yuta.hashiura@gmail.com>
2023-12-01 14:37:03 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  #authenticate_user!によりログインしていないユーザーをログインページの画面に促すことができます。
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private #ユーザーが新しくアカウントを作成するとき、名前（:name）といった情報も受け入れるように設定
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+
 end
